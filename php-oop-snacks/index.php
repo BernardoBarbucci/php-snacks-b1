f104aa3ab71a5d371085a09cce27658f0c335767
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-25 12:38:28 +0100

    5 proprietà

---
+++ php-oop-snacks/index.php
@@ -0,0 +1,19 @@
+<!-- 
+Create una nuova classe che rappresenti un Pokemon 
+    e che abbia almeno cinque proprieta,
+    tre metodi 
+    ed il costruttore.
+Microbonus: 
+    creo un'altra classe che aggiungo al pokemon per composizione
+    che configuri il tipo di pokemon (aria, terra, ecc.) 
+-->
+<?php
+
+class Pokemon
+{
+    public $nome;
+    public $tipo;
+    public $forza;
+    public $attacco;
+    public $difesa;
+}
