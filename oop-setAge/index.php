c226a9b3c372ba8d709bbf7591742ece26615696
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-30 09:43:15 +0100

    classe creata

---
+++ oop-setAge/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Parte 1:
+// Creare un nuovo oggetto Persona con un metodo setAge() che cambia il valore della proprieta' age
+// della classe Persona, che accetti esclusivamente un numero intero. 
+// Se il numero inserito come argomento non dovesse essere un numero intero: allora lanciare un'eccezione!
+
+class Persona
+{
+    public $age;
+}
